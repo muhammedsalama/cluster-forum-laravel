a728b88710a65aaec281cbaa6fcbde44543d341b
Mohamed Salama Shakir <salamam301@yahoo.com>
2019-07-29 17:57:32 +0200

    Complete social authentication

---
+++ app/Http/Controllers/SocialsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use function foo\func;
+use Illuminate\Http\Request;
+use SocialAuth;
+class SocialsController extends Controller
+{
+    public function auth($provider){
+        return SocialAuth::authorize($provider);
+    }
+
+    public function auth_callback($provider){
+        SocialAuth::login($provider,function ($user,$details){
+            $user->avatar = $details->avatar;
+            $user->email = $details->email;
+            $user->name = $details->full_name;
+            $user->save();
+        });
+        return redirect('/home');
+    }
+}
---
+++ config/eloquent-oauth.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\User;
+
+return [
+    'model' => User::class,
+    'table' => 'oauth_identities',
+    'providers' => [
+        'facebook' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/facebook/redirect',
+            'scope' => [],
+        ],
+        'google' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/google/redirect',
+            'scope' => [],
+        ],
+        'github' => [
+            'client_id' => 'a0d93f7ff5c0e17bd046',
+            'client_secret' => '8f0e232ff8bd9a524d737f6b5b0956ac0cb6039c',
+            'redirect_uri' => 'http://127.0.0.1:8000/github/redirect',
+            'scope' => [],
+        ],
+        'linkedin' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/linkedin/redirect',
+            'scope' => [],
+        ],
+        'instagram' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/instagram/redirect',
+            'scope' => [],
+        ],
+        'soundcloud' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/soundcloud/redirect',
+            'scope' => [],
+        ],
+    ],
+];
