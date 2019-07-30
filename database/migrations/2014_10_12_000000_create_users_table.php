9812f8047befeffa68e8d8bc4c6f09a0a8a369ef
Mohamed Salama Shakir <salamam301@yahoo.com>
2019-07-30 16:38:26 +0200

    Admin seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->boolean('admin')->default(0);
             $table->string('avatar');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
