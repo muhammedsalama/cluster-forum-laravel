5f6ea581717c7c250741aee04677929f731fefbb
Mohamed Salama Shakir <salamam301@yahoo.com>
2019-08-03 19:52:01 +0200

    Styling Views alittle bit

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -1,16 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>Forum</h2>
+
     @foreach($discussions as $disc)
+        <br>
         <div class="card">
             <div class="card-header">
-                <img src="{{$disc->user->avatar}}" alt="avatar" width="70px" height="70px">
-                {{$disc->title}}</div>
+                <img src="{{$disc->user->avatar}}" alt="avatar" width="45px" height="45px">&nbsp;
+                <span>{{$disc->user->name}}</span>
+                <a href="{{route('discussion',['slug'=>$disc->slug])}}" class="btn float-sm-right">view</a>
+            </div>
             <div class="card-body">
-                {{$disc->content}}
+                <h3 class="text-center">
+                    {{$disc->title}}
+                </h3>
+                <p class="card-text text-center">
+                    {{str_limit($disc->content,40)}}
+                </p>
+            </div>
+            <div class="card-footer">
+                @if($disc->replies->count() == 1)
+                    {{$disc->replies->count()}} <b>Reply</b>
+                @endif
+
+                @if($disc->replies->count() > 1)
+                    {{$disc->replies->count()}} <b>Replies</b>
+                @endif
             </div>
         </div>
+        <br>
     @endforeach
     <div class="text-center">
         {{$discussions->links()}}
