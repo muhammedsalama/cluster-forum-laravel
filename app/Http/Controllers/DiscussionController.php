da4c90f6a04796c9422ca736961c6e141ae45684
Mohamed Salama Shakir <salamam301@yahoo.com>
2019-08-09 15:09:27 +0200

    Set Laravel Mail Notifications

--- app/Http/Controllers/DiscussionController.php
+++ app/Http/Controllers/DiscussionController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Reply;
+use App\User;
 use Auth;
 use Illuminate\Http\Request;
 use App\Discussion;
@@ -9,51 +11,66 @@ use Session;
 
 class DiscussionController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         return view('discussions.create');
     }
 
-    public function store(){
+    public function store()
+    {
 
         $r = request();
 
-        $this->validate($r,[
-            'title'=>'required',
-            'content'=>'required',
-            'channel_id'=>'required'
+        $this->validate($r, [
+            'title' => 'required',
+            'content' => 'required',
+            'channel_id' => 'required'
         ]);
 
         $discussion = Discussion::create([
-            'title'=>$r->title,
-            'content'=>$r->content,
-            'channel_id'=>$r->channel_id,
-            'user_id'=>Auth::id(),
-            'slug'=>str_slug($r->title)
+            'title' => $r->title,
+            'content' => $r->content,
+            'channel_id' => $r->channel_id,
+            'user_id' => Auth::id(),
+            'slug' => str_slug($r->title)
         ]);
 
-       Session::flash('success','Discussion Created');
+        Session::flash('success', 'Discussion Created');
 
-       return redirect()->route('discussion',['slug'=>$discussion->slug]);
-
-    }
-
-    public function show($slug){
-        $disc = Discussion::where('slug',$slug)->first();
-        return view('discussions.show')->with('disc',$disc);
+        return redirect()->route('discussion', ['slug' => $discussion->slug]);
 
     }
 
-    public function reply($id , Request $request){
+    public function show($slug)
+    {
+        $disc = Discussion::where('slug', $slug)->first();
+        return view('discussions.show')->with('disc', $disc);
+
+    }
+
+    public function reply($id, Request $request)
+    {
 
         $d = Discussion::find($id);
 
+
         Reply::create([
-            'user_id'=> Auth::id(),
-            'discussion_id'=>$id,
-            'content'=>$request->reply
+            'user_id' => Auth::id(),
+            'discussion_id' => $id,
+            'content' => $request->reply
         ]);
 
-        Session::flash('success','Reply Added');
+
+        $watchers = array();
+
+        foreach ($d->watchers as $watcher):
+            $wat_user_id = User::find($watcher->user_id);
+            array_push($watchers, $wat_user_id);
+        endforeach;
+
+        dd($watchers);
+
+        Session::flash('success', 'Reply Added');
 
         return redirect()->back();
     }
